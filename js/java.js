a8e915c334bc7a5fd06ced3e240d44d65b4caf16
João Pedro Rodrigues dos Santos <maxxjotinha@gmail.com>
2021-11-24 09:39:28 -0300

    ainda ta com varios problemas

---
+++ js/java.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    
+    document.getElementById("voltar").addEventListener("click", function(){;
+        window.history.back();
+    });
+});
